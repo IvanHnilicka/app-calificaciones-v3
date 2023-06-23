5115bb95d4a3301b8a8312b95a8596b27c6a5e57
Ivan Hnilicka <ivan.hnilicka21@outlook.com>
2023-06-22 22:40:15 -0600

    Se quito highlight en boton al dar clic y otros ajustes en diseño. Se agrego android capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.voy-a-reprobar.app',
+  appName: '¿Voy a reprobar?',
+  webDir: 'dist/app-calificaciones-v3',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
